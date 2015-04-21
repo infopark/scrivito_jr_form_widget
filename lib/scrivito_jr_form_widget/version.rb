023904b2398a8e55f484c05cf9d2d40970affc05
Gert Geidel <gert.geidel@infopark.de>
2015-04-21 16:09:53 +0200

    New Version

--- lib/scrivito_jr_form_widget/version.rb
+++ lib/scrivito_jr_form_widget/version.rb
@@ -1,3 +1,3 @@
 module ScrivitoJrFormWidget
-  VERSION = "0.0.5"
+  VERSION = "0.0.6"
 end
--- scrivito/migrate/0_create_jr_form_widget.rb
+++ scrivito/migrate/0_create_jr_form_widget.rb
@@ -6,10 +6,11 @@ class CreateJrFormWidget < ::Scrivito::Migration
       title: 'Just Relate Fromular Widget',
       attributes: [
         {name: "activity_id", type: "string"},
+        {name: "event_id", type: "string"},
         {name: "subject", type: "string"},
         {name: "tags", type: "string"},
         {name: "redirect_to", type: "reference"},
-        {name: "submit_button_text", type: "string"}
+        {name: "submit_button_text", type: "string"},
       ]
     )
   end
